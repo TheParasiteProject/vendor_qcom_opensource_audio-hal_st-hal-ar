bfd253cf50454952a2516b20b0fd1a617f3e1469
SGCMarkus <markusornik@gmail.com>
2022-11-04 19:04:19 +0100

    st-hal-ar: use audio project pathmap for header includes
    Change-Id: Ia65b0823c3418bb79344c5ed33f639d4d80e98b7

--- Android.mk
+++ Android.mk
@@ -23,8 +23,8 @@ LOCAL_C_INCLUDES += \
     external/expat/lib \
     vendor/qcom/opensource/core-utils/fwk-detect \
     vendor/qcom/opensource/pal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal
+    $(call project-path-for,qcom-audio)/hal/audio_extn \
+    $(call project-path-for,qcom-audio)/hal
 
 LOCAL_SRC_FILES := \
     SoundTriggerDevice.cpp \
